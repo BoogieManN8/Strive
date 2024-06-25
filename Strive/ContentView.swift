fd2f601b2eb41ca6fb73615b2fb3e6dcc26a1f06
BoogieManN8 <tengizkiknadze8@gmail.com>
2024-06-25 07:35:53 +0400

    Initial Commit

---
+++ Strive/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Strive
+//
+//  Created by constantine Walker on 25.06.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Strive/StriveApp.swift
@@ -0,0 +1,17 @@
+//
+//  StriveApp.swift
+//  Strive
+//
+//  Created by constantine Walker on 25.06.24.
+//
+
+import SwiftUI
+
+@main
+struct StriveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
